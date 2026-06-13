425f5df8d64736fa386ef7780b67a0d38b381339
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2026-06-13 13:01:10 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -17,8 +17,8 @@ def new_page(playwright: Playwright, request):
         browser = playwright.webkit.launch(headless=headless)
     context = browser.new_context(viewport={"width": 1920, "height": 1080})
     page = context.new_page()
-    page.set_default_timeout(500000)  # 500 seconds
-    page.set_default_navigation_timeout(500000) # Increased timeouts due to Github Actions taking too long loading pages
+    page.set_default_timeout(60000)  # 60 seconds
+    page.set_default_navigation_timeout(60000) # Increase timeouts if page loads too long
     page.goto('https://academybugs.com/#')
     yield page
     browser.close()
